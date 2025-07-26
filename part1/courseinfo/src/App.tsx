ae83417d2d142cfc890330fee126b30714d56828
allrxx <alexjoy0480@gmail.com>
2025-07-26 15:21:32 +0530

    Excercise 1.3

--- part1/courseinfo/src/App.tsx
+++ part1/courseinfo/src/App.tsx
@@ -5,28 +5,37 @@ import Total from './comp/Total'
 const App = ()=>{
 
   const course = 'Half Stack Application Development'
-  const part1 = 'Fundamentals of react'
-  const exe1 = 10
-  const part2 = 'Using props to pass data'
-  const exe2 = 7
-  const part3 = 'State of a component '
-  const exe3 = 14
+  const part1 = {
+    name:'Fundamentals of react',
+    exe1: 10 
+  }
+
+  const part2 = { 
+    name: 'Using props to pass data',
+    exe2: 7 
+  } 
+
+  const part3 = {
+    name:'State of a component ',
+    exe3: 14
+  }
+ 
 
   return (
     <>
       <Header title={course}/>
       <ol>
         <li>
-          <Content content={part1} exe={exe1}/>
+          <Content content={part1.name} exe={part1.exe1}/>
         </li>
         <li>
-          <Content content={part2} exe={exe2}/>
+          <Content content={part2.name} exe={part2.exe2}/>
         </li>
         <li>
-          <Content content={part3} exe={exe3}/>
+          <Content content={part3.name} exe={part3.exe3}/>
         </li>
       </ol>
-      <Total total = {exe1+exe2+exe3}/>
+      <Total total = {part1.exe1 + part2.exe2 + part3.exe3}/>
     </>
   )
 }
